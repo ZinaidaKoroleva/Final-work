84e4340410364868abf7449f4d73f451616e9f8f
Zinaida <qktrey@gmail.com>
2022-05-08 19:22:57 +0300

    Find ArrayResultLenght

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Создание и заполненнение первоначального массива
+string [] arrayOriginal = new string [] {"hello", "2", "world", ":-)"};
+string [] arrayResult = new string [FindArrayResultLenght(arrayOriginal)];
+
+int FindArrayResultLenght (string [] array)
+{
+    int size = array.Length;
+
+    int arrayResultLenght = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayResultLenght++;
+        }
+    }
+    return arrayResultLenght;
+}
